f05d83b06d4f3acc0af0baa9cfe7715302871a77
Euan Riggans <euan@euanriggans.com>
2020-06-06 18:28:54 +0100

    Tidied up Snackbar.spec.js

--- tests/unit/Snackbar.spec.js
+++ tests/unit/Snackbar.spec.js
@@ -8,19 +8,21 @@ import Vue from 'vue'
 Vue.use(Vuetify)
 
 describe('Snackbar.vue', () => {
-
-    const wrapper = mount(Snackbar, {
-        propsData: {
-            color: 'pink',
-            modal: false,
-            text: 'Snackbar test message',
-            timeout: 5000
-        }
-    })
     it('sets the correct data when passed as props', () => {
+
+        const wrapper = mount(Snackbar, {
+            propsData: {
+                color: 'pink',
+                modal: false,
+                text: 'Snackbar test message',
+                timeout: 5000
+            }
+        })
+
         expect(wrapper.vm.color).toMatch('pink')
         expect(wrapper.vm.modal).toBeFalsy()
         expect(wrapper.vm.text).toMatch('Snackbar test message')
         expect(wrapper.vm.timeout).toBe(5000)
+        
     })
 })
